a46f4ecfc2764614af78fdc0a300b76388be5f76
Abyan Mohamed <abyanmohamed99@gmail.com>
2021-11-11 19:14:22 +0000

    Add more conditional examples

---
+++ tinkering.js
@@ -0,0 +1,23 @@
+function multiply(a, b) {
+    return a * b;
+}
+
+let result = multiply (2, 4);
+console.log(result);
+
+
+result = multiply (4, 6);
+console.log(result);
+
+console.log (2 + 10 - 19 + 4 - 90 +1)
+console.log(-20 + -19 - (-10) - (-1) + 24)
+console.log((10/5) * 4 - 20)
+console.log(4096 % 12)
+console.log(43 > 47)
+console.log(12 == 17)
+console.log(3 <= 3)
+console.log(1 != 0)
+console.log( "Hello" + 5*10)
+var quote = "Stay awhile and listen!";
+console.log(quote[6]);
+"3">1
